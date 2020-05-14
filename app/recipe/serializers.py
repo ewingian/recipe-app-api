6d7c0d835e68d45e7b03df06ed0b6a8639c6ed0f
Ian <ian.ewing@gmail.com>
2020-05-13 18:09:32 -0600

    Added custom recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -43,3 +43,9 @@ class RecipeSerializer(serializers.ModelSerializer):
             'tags', 'time_minutes', 'price', 'link'
         )
         read_only_fields = ('id',)
+
+# makes use of a nested serializer
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
